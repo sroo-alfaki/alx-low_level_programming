fb0195a4fa58693bdacca640d62810cbee0ce520
saria <srooalfakiahmed@gmail.com>
2023-09-07 18:52:57 -0700

    proj1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int y;
 long int z;
 long long int d;
 float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(x));
-printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(y));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(z));
-printf("size of a clong long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a char: %u byte(s)\n", (unsigned long)sizeof(x));
+printf("size of a int: %u byte(s)\n", (unsigned long)sizeof(y));
+printf("size of a long int: %u byte(s)\n", (unsigned long)sizeof(z));
+printf("size of a clong long int: %u byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %u byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
