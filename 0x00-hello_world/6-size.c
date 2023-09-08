dcadf319f41eff6910f9448574c47628f8675352
saria <srooalfakiahmed@gmail.com>
2023-09-07 19:43:14 -0700

    proj1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ long int z;
 long long int d;
 float f;
 printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(x));
-printf("Size of a int: %u byte(s)\n", (unsigned int)sizeof(y));
+printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(y));
 printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(z));
-printf("Size of a clong long int: %u byte(s)\n", (unsigned int)sizeof(d));
+printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(d));
 printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(f));
 return (0);
 }
