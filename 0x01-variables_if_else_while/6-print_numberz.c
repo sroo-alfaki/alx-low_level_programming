c85a493749c21dc3de21f385dab50f79f9300cb0
saria <srooalfakiahmed@gmail.com>
2023-09-10 17:39:06 -0700

    proj2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main -  entry point
+*
+* Description: print the value
+* Return:0 (Success)
+*/
+int main(void)
+{
+	int d = 0;
+
+	while
+		(d <= 9)
+		{
+			putchar(d + '0');
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
+
