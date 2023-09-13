25dc5b0a51bb4000d1fec33755d5915ad6ce8be7
saria <srooalfakiahmed@gmail.com>
2023-09-12 20:19:09 -0700

    proj3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int b;
+	int c;
+
+	for (b = 0; b <= 9; b++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+		_putchar('\n');
+	}
+}
