ff6e91ab23b72712f883006edc33ae24b4e21ad4
saria <srooalfakiahmed@gmail.com>
2023-09-12 18:00:44 -0700

    proj3

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main -  entry point
+*
+* Description : prints _putchar
+*
+* Return:0 (Success)
+*/
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+		_putchar(s[c]);
+	putchar('\n');
+	return (0);
+}
