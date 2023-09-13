fcf7d086ec292bd0ab5d4ebb77fdb5b2765eaca8
saria <srooalfakiahmed@gmail.com>
2023-09-12 19:43:30 -0700

    proj3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all the alphabet in lowercase - the function _putchar
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
