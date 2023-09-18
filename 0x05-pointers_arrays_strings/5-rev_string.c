2a2b5b39a94c7bb6c945bdc35ce41cccebdfd7f6
saria <srooalfakiahmed@gmail.com>
2023-09-18 16:41:44 -0700

    proj5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string -  function that prints a string
+ * @s: The value of pointer
+ *
+ * Return: 0 always
+ */
+void rev_string(char *s)
+{
+	int e, i;
+	char c;
+
+	for (e = 0; s[e] != '\0'; ++e)
+		;
+	for (i = 0; i < e / 2; i++)
+	{
+		c = s[e];
+		s[i] = s[e - 1 - i];
+		s[e - 1 - i] = c;
+	}
+}
