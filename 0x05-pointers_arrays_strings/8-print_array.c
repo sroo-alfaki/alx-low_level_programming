54d08a533b5df550ce52637c6201e68d3b8165e4
saria <srooalfakiahmed@gmail.com>
2023-09-18 17:49:43 -0700

    proj5

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,17 +5,17 @@
  * @a : value of pointer
  * @n : elements of an array
  *
- * Return: Always 0.
+ * Return: a and n.
  */
 void print_array(int *a, int n)
 {
-	printf("%d", *a);
-	printf(" , ");
-	printf("%d", *(a + 1));
-	printf(" , ");
-	printf("%d", *(a + 2));
-	printf(" , ");
-	printf("%d", *(a + 3));
-	printf(" , ");
-	printf("%d", *(a + 4));
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	if (i == (n - 1))
+		printf("%d", a[n - 1]);
+	printf("\n");
 }
