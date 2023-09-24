c9a3909cb29d799bf8160e02efe38c339555cfb7
saria <srooalfakiahmed@gmail.com>
2023-09-23 17:20:05 -0700

    proj8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,8 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	char a[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
+
+	write(2, a, sizeof(a));
 	return (1);
 }
