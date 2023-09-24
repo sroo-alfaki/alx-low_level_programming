053dfb43da59b34fa0c224b842a2144967c109dd
saria <srooalfakiahmed@gmail.com>
2023-09-23 17:25:59 -0700

    proj8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - entry point
  *
@@ -7,6 +8,7 @@
  *
  * Return: 1
  */
+
 int main(void)
 {
 	char a[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
