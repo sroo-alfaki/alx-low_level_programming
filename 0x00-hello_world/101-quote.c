36722eb2ecf27b24a584ee4ca5378f23ff8709c5
saria <srooalfakiahmed@gmail.com>
2023-09-23 17:37:28 -0700

    proj8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@
  *
  * Description :Write a C program that prints exactly
  *
- * Return: 1
+ * Return: program should return 1
  */
 
 int main(void)
 {
 	char a[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
 
-	write(1, a, sizeof(a[]));
+	write(1, a, sizeof(a));
 	return (1);
 }
