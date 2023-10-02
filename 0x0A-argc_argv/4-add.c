97201498de2562c7967815188f54a543bb58ef9a
saria <srooalfakiahmed@gmail.com>
2023-10-02 15:37:50 -0700

    p10

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		for (a = argv[argc]; *a; a++)
 			if (*a < '0' || *a > '9')
 				return (printf("Error\n"), 1);
-		s + = atoi(argv[argc]);
+		s += atoi(argv[argc]);
 	}
 	printf("%d\n", s);
 	return (0);
