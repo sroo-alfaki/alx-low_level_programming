eb65c68c204d81e29e1774f1a128edd9de3268f2
saria <srooalfakiahmed@gmail.com>
2023-10-02 16:45:56 -0700

    p10

---
+++ 0x07-pointers_arrays_strings/4-stp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  _strpbrk - function that searches a string for any of a set of bytes.
+ *
+ * @s :the string
+ * @accept : that matches one of the bytes
+ *
+ * Return: always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *a;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accpet[j] != '\0)
+		{
+			if (accept[j] == s[i])
+			{
+				a = &s[i];
+				return (a);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/5-sts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_chessboard : pa function that prints the chessboard.
+ *
+ * @a :rows
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
