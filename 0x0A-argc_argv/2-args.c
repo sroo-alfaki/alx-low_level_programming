5b610fc2380fdb8e189999c9bcd3346c623b279d
saria <srooalfakiahmed@gmail.com>
2023-10-02 14:47:04 -0700

    p10

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,12 +11,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	char *c;
+	int i = 0;
 
-	for (c = argv[argc - 1]; *c; c++)
+	while (argc--)
 	{
 		printf("%s\n", argv[i]);
+		i++;
 	}
 	return (0);
 }
