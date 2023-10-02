502f26b43abbbd0ed5a9d7640b543c15605807f1
saria <srooalfakiahmed@gmail.com>
2023-10-02 15:31:50 -0700

    p10

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description prints -  program that adds positive numbers
+ * @argc:count of  value in array
+ * @argv: array
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int s = 0;
+	char *a;
+
+	while (--argc)
+	{
+		for (a = argv[argc]; *a; a++)
+			if (*a < '0' || *a > '9')
+				return (printf("Error\n"), 1);
+		s + = atoi(argv[argc]);
+	}
+	printf("%d\n", s);
+	return (0);
+}
