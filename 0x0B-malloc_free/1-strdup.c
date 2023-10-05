43a130add5e4e0fa30ef8c4bdc0d802276156356
saria <srooalfakiahmed@gmail.com>
2023-10-04 17:37:23 -0700

    malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,14 +15,14 @@ char *_strdup(char *str)
 	int i = 0, s = 0;
 	char *n;
 
-	if (str == 0)
-		return (0);
+	if (str == NULL)
+		return (NULL);
 
 	for (; str[s] != '\0'; s++)
 		n = malloc(s * sizeof(*str) + 1);
 
 	if (n == 0)
-		return (0);
+		return (NULL);
 
 	else
 	{
