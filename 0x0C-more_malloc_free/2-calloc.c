7eade294e189e5aa1771156ab2b00fe6ee37458a
saria <srooalfakiahmed@gmail.com>
2023-10-08 17:01:08 -0700

    p

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	c = malloc(nmemb * size);
+	c = malloc(sizeof (int) * nmemb);
 	if (c == NULL)
 	return (NULL);
 	for (b = 0; b < (nmemb * size); b++)
