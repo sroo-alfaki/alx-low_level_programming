b51992ec92aa09e823b04e367e7d698f73f1cedc
saria <srooalfakiahmed@gmail.com>
2023-10-10 19:59:45 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ * @name:dag name
+ * @age: dag age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
