58c55f450a68a5fa4dc866d8f1092716b8745133
saria <srooalfakiahmed@gmail.com>
2023-10-11 17:35:23 -0700

    b

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer
+ * Return: Nothing.
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef Function_Pointers_H
+#define Function_Pointers_H
+
+#include <stdio.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
