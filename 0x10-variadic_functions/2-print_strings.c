47b4bcd221724b6b406c0ddd4a3f313ddb35b87d
saria <srooalfakiahmed@gmail.com>
2023-10-13 16:20:52 -0700

    v

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 
 
 /**
- * print_numbers - function that prints numbers, followed by a new line.
+ * print_strings - function that prints strings, followed by a new line.
  * @separator: the string
  * @n: numbers
  * @...: int to sum
@@ -10,7 +10,7 @@
  * Return: void
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list op;
