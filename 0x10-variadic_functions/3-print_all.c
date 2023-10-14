8fbde51ca6dbdcd184855e03befad9e3294ef99d
saria <srooalfakiahmed@gmail.com>
2023-10-14 04:03:30 -0700

    b

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,7 +22,7 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case'c':
-					printf("%s%c", sp, va_arg(op, char));
+					printf("%s%c", sp, va_arg(op, int));
 					break;
 				case'i':
 					printf("%s%d", sp, va_arg(op, int));
