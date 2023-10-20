e855da7c5838e6b4cfc081dc8fc887d662af8dc1
sroo-alfaki <srooalfakiahmed@gmail.com>
2023-10-20 11:08:39 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *size_t list_len - function that prints all the elements of a list_t list.
+ * @h: linked list of type listint_t to print
+ * Return: size
+ */
+
+
+size_t list_len(const list_t *h)
+{
+
+	size_t d = 0;
+
+	while (h)
+	{
+		h = h->next;
+		d++;
+	}
+
+	return (d);
+}
