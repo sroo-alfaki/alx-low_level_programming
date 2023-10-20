d0aada2514c8e9dcd8d3728749665ee6b6095945
sroo-alfaki <srooalfakiahmed@gmail.com>
2023-10-20 14:32:30 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,8 +9,9 @@
  *
  * Return: always 0
  */
-int main(int __attribute__((unused__)) argc, char *argv[])
+int main(int argc, char **argv[])
 {
+
 	int n1, n2;
 	char *op;
 
@@ -32,3 +33,48 @@ int main(int __attribute__((unused__)) argc, char *argv[])
 	printf("%d\n", get_op_func(op)(n1, n2));
 	return (0);
 }
+/*	int n1, n2, op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+
+
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(n1, n2));
+	return (0);
+}
+	if (((*argv[2] != '+') &&
+				(*argv[2] != '-') &&
+				(*argv[2] != '/') &&
+				(*argv[2] != '*') &&
+				(*argv[2] != '%') ||
+				(strlen(argv[2] != 1))
+				{
+				printf("Error\n");
+				exit(99);
+				}
+
+
+	if (*argv[3] == '0' && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+		}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	op = (*get_op_func(argv[2]))(n1, n2));
+	printf("%d\n", op);
+	return (0);
+}*/
