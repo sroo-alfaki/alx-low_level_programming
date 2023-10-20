e0fb0e1ad8efaa9678bcb66c127aea9ca7ef391d
sroo-alfaki <srooalfakiahmed@gmail.com>
2023-10-20 11:20:06 -0700

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,5 @@
 #include "lists.h"
 /**
- *_strlen - function length of string
- * @n: string
- * Return: int
- */
-int _strlen(char *n)
-{
-	int i = 0;
-
-	if (!n)
-		return (0);
-	while (*n++)
-		i++;
-	return (i);
-}
-/**
  *size_t list_len - function that prints all the elements of a list_t list.
  * @h: linked list of type listint_t to print
  * Return: size
@@ -23,11 +8,10 @@ int _strlen(char *n)
 
 size_t list_len(const list_t *h)
 {
-
 	size_t d = 0;
+
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 		d++;
 	}
