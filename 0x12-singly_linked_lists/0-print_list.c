54a73cb18a3ac8442ab71a8826bc2860d44d0090
sroo-alfaki <srooalfakiahmed@gmail.com>
2023-10-19 19:22:48 -0700

    b

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 /**
+ *_strlen - function length of string
+ * @n: string
+ * Return: int
+ */
+int _strlen(char *n)
+{
+	int i = 0;
+
+	if (!n)
+		return (0);
+	while (*n++)
+		i++;
+	return(i);
+}
+
+/**
  *print_listint - function that prints all the elements of a list_t list.
  * @h: linked list of type listint_t to print
  * Return: number of nodes
@@ -12,9 +28,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%d\n", h->n);
-		d++;
+		printf("%d\n", _strlen(h->str), h->str ? h-> : "(nil)");
 		h = h->next;
+		d++;
 	}
 
 	return (d);
