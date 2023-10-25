64db159a75f380a0d03ffd7fc9dd833517600b98
saria <srooalfakiahmed@gmail.com>
2023-10-26 01:30:31 +0300

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,9 +8,10 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t i;
 
-	while (h)
+	i = 0;
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-print_listint(const listint_t *h);
+size_t pirint_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -32,6 +32,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
+
 #endif
