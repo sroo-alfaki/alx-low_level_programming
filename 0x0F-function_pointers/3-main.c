8a2abfb98696970b6810c14fd1a3a3fb5422458a
saria <srooalfakiahmed@gmail.com>
2023-11-29 07:18:39 -0800

    function_pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+#include "function_pointers.h"
+/**
+ * main - print result
+ * @argc: number
+ * @argv: array
+ *
+ * Return: always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+
+	int n1, n2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && n2 == 0) || (*op == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(n1, n2));
+	return (0);
+}
