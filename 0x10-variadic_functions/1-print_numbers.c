3d69098fdac5ab965a661afd28bb394298c88002
saria <srooalfakiahmed@gmail.com>
2023-11-30 14:40:20 -0800

    var

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line.
+ * @separator: the string
+ * @n: numbers
+ * @...: int to sum
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list op;
+
+	va_start(op, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(op, int));
+				if (i != (n - 1) && separator != NULL)
+				printf("%s", separator);
+				}
+					printf("\n");
+					va_end(op);
+}
