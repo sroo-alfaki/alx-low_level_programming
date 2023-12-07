98b1426de803376232630f74a91d243e117db09f
saria <srooalfakiahmed@gmail.com>
2023-12-07 13:47:00 -0800

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *list_len - function that prints all the elements of a list_t list.
+ * @h: linked list of type listint_t to print
+ * Return: size
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t d = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		d++;
+	}
+
+	return (d);
+}
