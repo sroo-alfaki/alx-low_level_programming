55153ec41fde1d2f09f16c64bafecac0369e3121
saria <srooalfakiahmed@gmail.com>
2024-05-22 18:24:33 -0700

    search_algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -25,11 +25,11 @@ int binary_search(int *array, size_t size, int value)
 
 		if (array[mid] == value)
 			return mid;
+
 		else if (array[mid] < value)
 			left = mid + 1;
 		else
 			right = mid - 1;
 	}
-
 	return -1;
 }
